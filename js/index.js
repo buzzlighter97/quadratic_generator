97e26b68db24c23cae68ac2face0b006ee9bc8ac
Ivan Ivanov <buzzlighter97@gmail.com>
2021-09-09 23:01:07 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -23,40 +23,44 @@ function generateEquation() {
     let left_edge = document.forms['edges']['left-edge'].value;
     let right_edge = document.forms['edges']['right-edge'].value;
 
-    numbers = generateNumbers(left_edge, right_edge);
-    let a = numbers[0];
-    let b = numbers[1];
-    let c = numbers[2];
-    let D = Math.pow(b,2) - 4*a*c;
-    while ((!Number.isInteger(Math.sqrt(D))) || (D<0) || (c===0)) {
+    if (left_edge<=-2 && right_edge>=2) {
         numbers = generateNumbers(left_edge, right_edge);
-        a = numbers[0];
-        b = numbers[1];
-        c = numbers[2];
-        D = Math.pow(b,2) - 4*a*c;
-    };
+        let a = numbers[0];
+        let b = numbers[1];
+        let c = numbers[2];
+        let D = Math.pow(b,2) - 4*a*c;
+        while ((!Number.isInteger(Math.sqrt(D))) || (D<0) || (c===0)) {
+            numbers = generateNumbers(left_edge, right_edge);
+            a = numbers[0];
+            b = numbers[1];
+            c = numbers[2];
+            D = Math.pow(b,2) - 4*a*c;
+        };
 
-    if (a===-1) {
-        document.getElementById('a-value').innerHTML = '-';
-    } else if (a===1) {
-        document.getElementById('a-value').innerHTML = ' ';
-    } else {
-        document.getElementById('a-value').innerHTML = a.toString(10);
-    }
+        if (a===-1) {
+            document.getElementById('a-value').innerHTML = '-';
+        } else if (a===1) {
+            document.getElementById('a-value').innerHTML = ' ';
+        } else {
+            document.getElementById('a-value').innerHTML = a.toString(10);
+        }
 
-    if (b===-1) {
-        document.getElementById('b-value').innerHTML = '-';
-    } else if (b===1) {
-        document.getElementById('b-value').innerHTML = '+';
-    } else if (b>0) {
-        document.getElementById('b-value').innerHTML = '+' + b.toString(10);
-    } else {
-        document.getElementById('b-value').innerHTML = b.toString(10);
-    }
+        if (b===-1) {
+            document.getElementById('b-value').innerHTML = '-';
+        } else if (b===1) {
+            document.getElementById('b-value').innerHTML = '+';
+        } else if (b>0) {
+            document.getElementById('b-value').innerHTML = '+' + b.toString(10);
+        } else {
+            document.getElementById('b-value').innerHTML = b.toString(10);
+        }
 
-    if (c>0) {
-        document.getElementById('c-value').innerHTML = '+' + c.toString(10);
+        if (c>0) {
+            document.getElementById('c-value').innerHTML = '+' + c.toString(10);
+        } else {
+            document.getElementById('c-value').innerHTML = c.toString(10);
+        };
     } else {
-        document.getElementById('c-value').innerHTML = c.toString(10);
+        return
     }
 };
